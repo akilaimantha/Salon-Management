ed0c5d3007bfe94809bdd409f42a5cf6455067cf
RanugiJang <ranugigihansa@gmail.com>
2025-03-24 23:45:55 +0530

    create appointments

--- api/index.js
+++ api/index.js
@@ -2,27 +2,48 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import dbConnect from "./config/db.js";
-//import authRoutes from "./routes/authRoute.js";
+import authRoutes from "./routes/authRoute.js";
 import cookieParser from "cookie-parser";
 import inventory from "./routes/inventoryRoute.js";
-import appointment from "./routes/appoimentRouts.js";
-import feedback from "./routes/feedbackRoute.js";
+import serviceRoutes from "./routes/Service_Route.js";
+import appoinment from "./routes/appoimentRouts.js";
 import Package from "./routes/PackageRoute.js";
-import Service from "./routes/Service_Route.js";
+import cors from "cors";
+import feedback from "./routes/feedbackRoute.js";
+import { fileURLToPath } from "url"; // Import to convert URL to file path
+import { dirname, join } from "path"; // Import to work with paths
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-//app.use("/api/v1/auth", authRoutes); 
-app.use("/api/v1/appoiment", appointment); 
-app.use("/api/v1/feedback", feedback); 
-app.use("/api/v1/Package",Package);
-app.use("/api/v1/Service",Service);
+app.use(express.json()); 
+app.use(cors({ 
+ origin: 'http://localhost:5173',
+ credentials: true,
+ allowedHeaders: ['Content-Type'],
+ optionsSuccessStatus: 200,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type'], 
+}));
+
+// Calculate __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url); // Get the current file's path
+const __dirname = dirname(__filename); // Get the directory name from the file path
+
+// Serve static files from the uploads folder
+app.use("/uploads", express.static(join(__dirname, "uploads")));
+
+
+app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/inventory", inventory);
-  
-const PORT = 7002;
+app.use("/api/services", serviceRoutes);
+app.use("/api/v1/appoiment", appoinment);
+app.use("/api/v1/package", Package);
+app.use("/api/v1/feedback", feedback);
+
+const PORT = 7001;
 
 dbConnect()
   .then(() => {
@@ -32,7 +53,5 @@ dbConnect()
   })
   .catch((error) => {
     console.error("Database connection failed:", error);
-    process.exit(1); // Stop the app if DB fails to connect
+    process.exit(1);
   });
-
-  
